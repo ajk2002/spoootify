5b1cc6bcfdd08bdff7720f435b78414cb864a267
Aaron Kannenkeril <aaronkannenkeril@Aarons-Mac-mini.local>
2025-08-29 14:30:52 +0100

    one way and all assets

--- data.js
+++ data.js
@@ -115,5 +115,40 @@ const SONGS = [
       ]
       // Or use: text: `...` (if you don’t want timestamps)
     }
+  },
+  {
+    id: "song3",
+    title: "ONE WAY",
+    cover: "assets/ONEWAY/IMG_6420.jpg",
+    audio: "assets/ONEWAY/ONEWAY.wav", 
+    duration: "1:46",
+    lyrics: {
+      timed: [
+        { t: 13.5, line: "How much more blessed can I be? " },
+        { t: 16.3, line: "When I see you, yh I feel it in my jeans " },
+        { t: 19.8, line: "Your eyes make me feel seen" },
+        { t: 22.5, line: "It's a special feeling, do you know what I mean?" },
+        { t: 26.5, line: "You're my destiny" },
+        { t: 32.5, line: "I swear you're meant for me " },
+        { t: 39.5, line: "So many people walking by with different lives " },
+        { t: 43, line: "I'm just glad that I caught you at the right time" },
+        { t: 46.8, line: "Now it's been a while, I still feel the same way" },
+        { t: 49.5, line: "Like were on a flight and it's only one way" },
+        { t: 52.7 , line: "One way, one way, nowadays I can only see you one way" },
+        { t: 57.5, line: "With a heart full of love, that's my baby" },
+        { t: 60, line: "Think you might be the key yh you saved me " },
+        { t: 65.5, line: "And you opened up doors" },
+        { t: 67, line: "That I never knew existed for me" },
+        { t: 70, line: "Light on the other side, feeling so free" },
+        { t: 76.6, line: "I swear we'd make cute babies" },
+        { t: 80, line: "Feeling sentimental, when I think of you" },
+        { t: 83, line: "Dreaming bout the future and what we could do" },
+        { t: 86.7, line: "Flat in the city or house on the land" },
+        { t: 90, line: "It ain't nothin without us two" },
+        
+        
+      ]
+      // Or use: text: `...` (if you don’t want timestamps)
+    }
   }
 ];
